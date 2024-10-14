fe6f2b820dde079bd4dafeaa31fdca43adf6189c
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-15 01:58:58 +0900

    Title: 알파벳 출력하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241015/알파벳 출력하기/print-alphabet.py
+++ 241015/알파벳 출력하기/print-alphabet.py
@@ -4,9 +4,9 @@ count = 0
 
 for i in range(n):
     for j in range(i+1):
-        print(chr(ord(start)+count), end="")
         if count <= 26:
             count+=1
         else:
-            count-=26
+            count-=27
+        print(chr(ord(start)+count), end="")
     print()
