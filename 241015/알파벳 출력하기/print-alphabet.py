c8fde8759d67bfada2898f1ebbd42482d83cbc94
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-15 02:03:43 +0900

    Title: 알파벳 출력하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/알파벳 출력하기/print-alphabet.py
+++ 241015/알파벳 출력하기/print-alphabet.py
@@ -1,12 +1,10 @@
 n = int(input())
-start = 'A'
-count = -1
+cnt = 'A'
 
 for i in range(n):
-    for j in range(i+1):
-        if count <= 24:
-            count+=1
-        else:
-            count-=25
-        print(chr(ord(start)+count), end="")
-    print()
+	for _ in range(i + 1):
+		print(cnt, end="")
+		cnt = chr(ord(cnt) + 1)
+		if ord(cnt) > ord('Z'):
+			cnt = 'A'
+	print()
