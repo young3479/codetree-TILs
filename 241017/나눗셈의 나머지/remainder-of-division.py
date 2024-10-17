5b9bae71fdfed5c61ba454e1453153a40540385a
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-17 10:37:58 +0900

    Title: 나눗셈의 나머지, Time: 196ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/나눗셈의 나머지/remainder-of-division.py
+++ 241017/나눗셈의 나머지/remainder-of-division.py
@@ -3,8 +3,8 @@ count = [0 for _ in range(10)] #나머지는 1~9임
 answer = 0
 
 while a > 1: #a가 1보다 클동안 수행할 작업
-    a = a//b
     count[a%b] += 1
+    a = a//b
 
 for i in count:
     answer = answer + i**2
