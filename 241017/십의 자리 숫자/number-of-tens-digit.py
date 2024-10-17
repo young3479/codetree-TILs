806d5886f4f0ff3248e88706bc70937a7f0c258b
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-17 09:58:54 +0900

    Title: 십의 자리 숫자, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/십의 자리 숫자/number-of-tens-digit.py
+++ 241017/십의 자리 숫자/number-of-tens-digit.py
@@ -1,12 +1,11 @@
 n = list(map(int, input().split()))
-count = [0 for _ in range(9)]
+count = [0 for _ in range(10)]
 for i in n:
-    if i != 0:
-        if len(str(i)) != 1:
-            i = int(i//10)
-            count[i-1] += 1
-    else:
+    if i == 0:
         break
+    if i < 10:
+        continue
+    count[i//10] += 1
 
 for j in range(1, 10):
-    print(f"{j} - {count[j-1]}")
+    print(f"{j} - {count[j]}")
