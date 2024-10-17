f770450d8889ebe3f7a3bca5771d81f1696038c8
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-17 12:45:12 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241017/배열 놀이/play-with-array.py
+++ 241017/배열 놀이/play-with-array.py
@@ -1,7 +1,3 @@
-#n개의 원소와 q개의 질의가 주어짐
-#n개의 원소값이 차례대로 공백으로 구분
-#q개의 줄에 걸쳐 질의 주어짐
-
 n, q = map(int, input().split())
 nums = list(map(int, input().split()))
 question = [list(map(int, input().split())) for _ in range(q)]
