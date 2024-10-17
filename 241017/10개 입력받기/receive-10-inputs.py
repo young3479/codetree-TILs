b3ac75be4d93a52afde4c64952bf95e4c23b5fc9
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-17 09:12:21 +0900

    Title: 10개 입력받기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,13 @@
+n = list(map(int, input().split()))
+answer = 0
+count = 0
+
+for i in n:
+    if i != 0:
+        answer += i
+        count += 1
+
+    else:
+        break
+
+print(answer, answer/count)
