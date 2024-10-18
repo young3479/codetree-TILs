c80490ec13b02de2ffebabc771f6e15558ba0e43
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-18 22:37:09 +0900

    Title: 999 또는 -999, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/999 또는 -999/999-or-999.py
+++ 241018/999 또는 -999/999-or-999.py
@@ -4,9 +4,9 @@ min_num = 999
 for i in nums:
     if i == 999 or i == -999:
         break
-    elif i >= max_num:
+    elif i > max_num:
         max_num = i
-    elif i <= min_num:
+    elif i < min_num:
         min_num = i
 
 print(max_num, min_num)
