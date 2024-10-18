b9a11c3aa2de6fdc2c2298b00972ccdeea7254d4
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-18 22:36:10 +0900

    Title: 999 또는 -999, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/999 또는 -999/999-or-999.py
@@ -0,0 +1,12 @@
+nums = list(map(int, input().split()))
+max_num = -999
+min_num = 999
+for i in nums:
+    if i == 999 or i == -999:
+        break
+    elif i >= max_num:
+        max_num = i
+    elif i <= min_num:
+        min_num = i
+
+print(max_num, min_num)
