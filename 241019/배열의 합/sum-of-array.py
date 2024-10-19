a42e110c38658c5f1955028d8d9f17eed5f81953
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-19 15:32:59 +0900

    Title: 배열의 합, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 241019/배열의 합/sum-of-array.py
+++ 241019/배열의 합/sum-of-array.py
@@ -1,7 +1,3 @@
 map = [list(map(int, input().split())) for _ in range(4)]
-answer = 0
 for i in range(4):
-    for j in range(4):
-        answer += map[i][j]
-    print(answer)
-    answer = 0
+    print(sum(map[i]))
