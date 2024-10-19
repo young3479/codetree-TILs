3b52c94a2fd7f7d902a4dd7b1f4366f3ae95722e
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2024-10-19 15:03:04 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241019/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 241019/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -4,11 +4,11 @@ two_count = 0
 for i in range(n):
     if two_count == 3:
         print(nums.index(nums[i]))
+        # print("카운트 3일때", i)
         break
-
     if nums[i] == 2:
         two_count += 1
+        # print("2일때", i)
         continue
-
     else:
         continue
