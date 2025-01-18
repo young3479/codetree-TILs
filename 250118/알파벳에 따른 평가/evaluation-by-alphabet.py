556eb24afd8653c8367f1fb8d2becf3b9ea6fea2
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-01-18 14:19:01 +0900

    Title: 알파벳에 따른 평가, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250118/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+ch = input()
+if ch == 'S':
+    print("Superior") 
+elif ch == 'A':
+    print("Excellent")
+elif ch == 'B':
+    print("Good")
+elif ch == 'C':
+    print("Usually")
+elif ch == 'D':
+    print("Effort")
+else:
+    print("Failure")
