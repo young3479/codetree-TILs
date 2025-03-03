a6fa52cd445234f24fd05efb82d4fd726d2277b8
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-03-03 22:33:30 +0900

    Title: DateTime to DateTime, Time: 5052ms, Memory: 16MB, Status: Time Limit Exceed - CodeTree

--- 250303/DateTime to DateTime/datetime-to-datetime.py
+++ 250303/DateTime to DateTime/datetime-to-datetime.py
@@ -1,27 +1,25 @@
-# 11월 11일 11시 11분에서 
+# 11월 11일 11시 11분에서
 # 11월 A일 B시 C분까지 몇분이 걸리는 지
 # 우선 분이 60보다 크면 시간 하나 증가
-# 시간이 24보다 크면 일이 하나 증가 
+# 시간이 24보다 크면 일이 하나 증가
 # 시간이 11, 11, 11 에서 시작하여 A, B, C와 동일할 때까지 진행
 
 A, B, C = map(int, input().split())
 
 # 단 11,11,11 보다 앞서면 -1 출력
 
-if A < 11 and B < 11 and C < 11:
-    print(-1)
-elif A == 11 and B < 11:
-    print(-1)
-elif A == 11 and B == 11 and C < 11:
-    print(-1)
+if A <= 11:
+    if B <= 11:
+        if C < 11:
+            print(-1)
 
 day, hour, minute = 11, 11, 11
 cnt = 0
 
-while(True):
+while (True):
     if day == A and hour == B and minute == C:
         break
-    
+
     if minute == 60:
         hour += 1
         minute = 0
