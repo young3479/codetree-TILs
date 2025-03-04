94e8ed025b0f990da4388731f8fbacc3a818be50
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-03-04 23:46:32 +0900

    Title: 최대로 겹치는 지점, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250304/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+info = [list(map(int, input().split())) for _ in range(N)]
+
+# print(info)
+
+dp = [0] * 100 #1부터 담기
+
+for s, e in info:
+    for i in range(s, e+1):
+        dp[i] += 1
+        
+print(max(dp))
