49a374eab5d743fce03da1e1911da649b4f3eba3
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-03-10 18:03:21 +0900

    Title: 되돌아오기 2, Time: 76ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250310/되돌아오기 2/come-back-2.py
@@ -0,0 +1,29 @@
+info = list(input())
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+x, y = 0, 0 # 초기 좌표 위치
+direct = 0 # 북쪽 바라보고 시작
+time = 0
+
+for i in info:
+    if i == 'F':
+        x = x + dx[direct]
+        y = y + dy[direct]
+        time += 1
+
+    elif i == 'L':
+        direct = (direct + 3) % 4 # 명령 L일 때
+        time += 1
+
+    elif i == 'R':
+        direct = (direct + 1) % 4 # 명령 R일 때 
+        time += 1
+    
+    else:
+        print("잘못된 입력입니다.")
+
+    if x == 0 and y == 0:
+        break
+
+print(time)
+
