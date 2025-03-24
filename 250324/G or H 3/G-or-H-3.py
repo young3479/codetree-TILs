04744eb08c1c28b0e0569cfad79d84a82336fe65
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-03-24 19:14:41 +0900

    Title: G or H 3, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250324/G or H 3/G-or-H-3.py
+++ 250324/G or H 3/G-or-H-3.py
@@ -11,16 +11,16 @@ dp = [0] * (max(x)+1)
 for i in range(n):
     if c[i] == 'G':
         dp[x[i]] += 1
-    else:
+    elif c[i] == 'H':
         dp[x[i]] += 2
-
-
+        
 ans = 0
-for i in range(1, len(dp)-k):
+for i in range(1, len(dp)-k+2):
     dist = 0
     dist = sum(dp[i:i+k+1])
 
     ans = max(dist, ans)
 
 print(ans)
+
 
