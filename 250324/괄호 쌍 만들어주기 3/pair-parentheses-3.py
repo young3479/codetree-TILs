ab42d7b412e834166e8efcaea6417859abb6665c
Park ChaeYoung <127152443+young3479@users.noreply.github.com>
2025-03-24 17:49:15 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250324/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,10 @@
+A = list(input())
+
+cnt = 0
+for i in range(len(A)):
+    if A[i] == '(':
+        for j in range(i+1, len(A)):
+            if A[j] == ')':
+                cnt += 1
+
+print(cnt)
